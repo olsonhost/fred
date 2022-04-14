c8cbc460da518e7c8ab3964522ea8535a76420d0
Erik Olson <erik@Olson.host>
2022-04-14 11:35:33 -0400

    wip

--- src/Init.php
+++ src/Init.php
@@ -1,46 +1,19 @@
 <?php 
 
-namespace Olsonhost\Ect;
+namespace Olsonhost\Phat;
 
-// Return a fully initialized Ect Object which can
-//	Execute a command (sendSMS, dial, etc)
-//	Process a webhook
-//	Check for scheduled or queued tasks
-
-class Init
+class Phat
 {
-
-	public $tel;
 
 	public function __construct()
 	{
 
-		// Assign our preferred telephony driver to $tel
 
-		$this->tel = new Twilio($this);	
 	}
 
 	public function test() {
 
-		// $tel->webhook(false) should callback this
-
-		echo '*** TEST!!! ***';
-	}
-
-	public function whtest() {
-
-		// Test $tel->webhook(), should callback to test()
-
-		$this->tel->webhook();
-
-	}
-
-	public function sendSMS($to, $from, $message) {
-		$this->tel->sendSMS($to, $from, $message);
-	}
-
-	public function dial($number, $callback) {
-		$this->tel->dial($number, $callback);
+		echo '*** PHAT TEST!!! ***';
 	}
 
 }
